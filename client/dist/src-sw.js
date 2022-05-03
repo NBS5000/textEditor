28f08424ae452ad95b65cb2a9c3ef1914c4f0c7c
NBS5000 <steve.nbs.barry@gmail.com>
2022-05-03 19:41:43 +1000

    test

--- client/dist/src-sw.js
+++ client/dist/src-sw.js
@@ -6,6 +6,7 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
+
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
